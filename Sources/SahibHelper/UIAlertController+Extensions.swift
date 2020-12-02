752b0f84eac7cdc13c921f4d8821bebdeb1f8391
sahib hussain <sahib.hussain@retailogy.com>
2020-12-02 21:10:55 +0530

    alertcontroller

--- Sources/SahibHelper/UIAlertController+Extensions.swift
+++ Sources/SahibHelper/UIAlertController+Extensions.swift
@@ -2,8 +2,40 @@ import UIKit
 import AudioToolbox
 
 // MARK: - Methods
-public extension UIAlertController {
+extension UIAlertController {
     
+    /// Add an action to Alert
+    ///
+    /// - Parameters:
+    ///   - title: action title
+    ///   - style: action style (default is UIAlertActionStyle.default)
+    ///   - isEnabled: isEnabled status for action (default is true)
+    ///   - handler: optional action handler to be called when button is tapped (default is nil)
+    func addAction(image: UIImage? = nil, title: String, color: UIColor? = nil, style: UIAlertAction.Style = .default, isEnabled: Bool = true, handler: ((UIAlertAction) -> Void)? = nil) {
+        //let isPad: Bool = UIDevice.current.userInterfaceIdiom == .pad
+        //let action = UIAlertAction(title: title, style: isPad && style == .cancel ? .default : style, handler: handler)
+        let action = UIAlertAction(title: title, style: style, handler: handler)
+        action.isEnabled = isEnabled
+        
+        // button image
+        if let image = image {
+            action.setValue(image, forKey: "image")
+        }
+        
+        // button title color
+        if let color = color {
+            action.setValue(color, forKey: "titleTextColor")
+        }
+        
+        addAction(action)
+    }
+    
+    /// Set alert's title, font and color
+    ///
+    /// - Parameters:
+    ///   - title: alert title
+    ///   - font: alert title font
+    ///   - color: alert title color
     func set(title: String?, font: UIFont, color: UIColor) {
         if title != nil {
             self.title = title
@@ -53,4 +85,3 @@ public extension UIAlertController {
     }
     
 }
-
